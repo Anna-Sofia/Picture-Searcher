93086765562eab1ea6c3ebc39e773692f216389f
Anna-Sofia <Anna-Sofia@users.noreply.github.com>
2019-03-24 01:02:17 +0200

    Search feedback on Ui and fixes

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,25 @@ import Axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        Axios.get('https://api.unsplash.com/search/photos', {
+
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await Axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
-                Authorization: 'Client-ID 97fca4a595f11dd6e28d8a757c6e620e32ec63c9a156fd7c364fe9e2a2da620b'
+            Authorization:
+                'Client-ID 97fca4a595f11dd6e28d8a757c6e620e32ec63c9a156fd7c364fe9e2a2da620b'
             }
         });
+        this.setState({ images: response.data.results });
     }
-
+    
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         );
     }
