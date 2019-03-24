3c18c67fbc7d37328b62da29b6450aa82ef02537
Anna-Sofia <Anna-Sofia@users.noreply.github.com>
2019-03-24 23:23:33 +0200

    Temporary Heroku deploy init push

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
         Authorization:
-//            'Put your cliend id here'
+            'Client-ID 97fca4a595f11dd6e28d8a757c6e620e32ec63c9a156fd7c364fe9e2a2da620b'
         }
 });
